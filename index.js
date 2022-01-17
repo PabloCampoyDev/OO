f159b022ee5bf26404e011983656055325642522
pablo_campoy <pablo@orasystems.com.br>
2022-01-17 09:54:13 -0300

    trabalhando com metodos da classe

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo = "0.00";
 }
 
-const client1 = new Cliente();
-const client2 = new Cliente();
+class ContaCorrete{
+    agencia;
+    saldo = 0.00;
+    sacar(valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
 
+const contacorrentePablo = new ContaCorrete(); 
+contacorrentePablo.agencia = 1001;
+console.log(contacorrentePablo.saldo);
+contacorrentePablo.saldo = 100;
+console.log(contacorrentePablo.saldo);
+let sacado = 50;
+contacorrentePablo.sacar(sacado);
+console.log(contacorrentePablo.saldo);
+
+const client1 = new Cliente();
 client1.nome = "Pablo";
 client1.cpf = "403.995.568-42";
-client1.agencia = 0425;
-client1.saldo = 250.35;
 
+const client2 = new Cliente();
 client2.nome = "Enilson";
 client2.cpf = "425.995.568-42";
-client2.agencia = 123;
 
-console.error(client1, client2);
+console.log(client1)
+console.log(client2);
